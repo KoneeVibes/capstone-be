fdefb5cf63104510bd9b0ac8aa057ca30251a2f6
Konee <104918313+KoneeVibes@users.noreply.github.com>
2026-08-10 00:53:08 +0100

    updated the role field with validator to ensure it only applies to type of staff

--- model/user.ts
+++ model/user.ts
@@ -47,6 +47,21 @@ const userSchema = new Schema(
 			type: String,
 			default: null,
 			enum: ["super-admin", "admin", "manager", "regular"],
+			validate: {
+				validator: function (value: string | null) {
+					if (this.type === "staff") {
+						return ["super-admin", "admin", "manager", "regular"].includes(
+							value ?? "",
+						);
+					}
+					return value === null;
+				},
+				message(this: any): string {
+					return this.type === "staff"
+						? "Staff users must have a valid role: super-admin, admin, manager, or regular."
+						: "Only staff users can have a role.";
+				},
+			},
 		},
 		organization: {
 			type: String,
