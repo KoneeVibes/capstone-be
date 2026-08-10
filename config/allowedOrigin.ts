4974906540b3e2164ceaab6c0a7f32cde4cc4d19
Konee <104918313+KoneeVibes@users.noreply.github.com>
2026-08-10 14:11:50 +0100

    added deployed server url to allowed origins

--- config/allowedOrigin.ts
+++ config/allowedOrigin.ts
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+	"http://localhost:5173",
+	"https://capstoneglobalhq.com",
+	"https://capstone-be-1hri.onrender.com",
+];
 
 export default allowedOrigins;
